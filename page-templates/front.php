192d1c6fcb56e0019e43300685c61efa32b3a1b6
Ben Owen <ben@Bens-iMac-2.local>
2017-08-10 17:42:54 -0500

    Homepage slides - finishing touchse

--- page-templates/front.php
+++ page-templates/front.php
@@ -5,47 +5,52 @@ Template Name: Front
 get_header(); ?>
 
 	<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-	  	<div class="orbit-wrapper">
-	    	<div class="orbit-controls">
-	      		<button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-	      		<button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-	    	</div>
-	    	<ul class="orbit-container">
-	      		<li class="is-active orbit-slide">
-	        		<figure class="orbit-figure">
-	          			<img class="orbit-image" src="http://placehold.it/1200x600/999?text=Slide-1" alt="Space">
-	          			<figcaption class="orbit-caption">Space, the final frontier.</figcaption>
-	        		</figure>
-	      		</li>
-	      		<li class="orbit-slide">
-	        		<figure class="orbit-figure">
-	          			<img class="orbit-image" src="http://placehold.it/1200x600/888?text=Slide-2" alt="Space">
-	          			<figcaption class="orbit-caption">Lets Rocket!</figcaption>
-	        		</figure>
-				</li>
-				<li class="orbit-slide">
-					<figure class="orbit-figure">
-						<img class="orbit-image" src="http://placehold.it/1200x600/777?text=Slide-3" alt="Space">
-						<figcaption class="orbit-caption"><h3>Encapsulating</h3> Outta This World, Outta This World.</figcaption>
-					</figure>
-				</li>
-	    	</ul>
-		</div>
-	  	<nav class="orbit-bullets">
-	    	<button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-	    	<button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-	    	<button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-	  	</nav>
+		<?php if( have_rows('slides') ): ?>
+			<div class="orbit-wrapper">
+				<div class="orbit-controls">
+  					<button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+			      	<button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+			    </div>
+			    <ul class="orbit-container">
+			    <?php while ( have_rows('slides') ) : the_row();
+				$image = get_sub_field('image');
+				?>
+					<li class="orbit-slide">
+						<figure class="orbit-figure">
+							<img class="orbit-image" src="<?php echo $image; ?>" alt="<?php the_sub_field('title'); ?>">
+							<figcaption class="orbit-caption">
+								<h3><?php the_sub_field('title'); ?></h3>
+								<?php the_sub_field('description'); ?>
+								<a href="<?php the_sub_field('link'); ?>" class="button yellow"><?php the_sub_field('button'); ?></a>
+							</figcaption>
+						</figure>
+					</li>
+				<?php endwhile; ?>
+				</ul>
+			</div>
+			<nav class="orbit-bullets">
+			<?php
+			$slide = 0;
+			while ( have_rows('slides') ) : the_row(); ?>
+				<button class="<?php echo ($slide == 0 ? 'is-active' : ''); ?>" data-slide="<?php echo $slide; ?>"><span class="show-for-sr"><?php the_sub_field('title'); ?></span></button>
+			<?php
+			++$slide;
+			endwhile;
+			?>
+			</nav>
+			<?php else :
+					echo "You need to setup slides for this page, or choose a different template.";
+				endif;
+				?>
 	</div>
-
-	<?php get_sidebar(); ?>
-
-	<?php do_action( 'foundationpress_before_content' ); ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-	<section class="main-wrap sidebar-left" role="main">
-		<?php the_content(); ?>
-	</section>
-	<?php endwhile;?>
-	<?php do_action( 'foundationpress_after_content' ); ?>
-
+	<div class="main-wrap sidebar-left">
+		<?php do_action( 'foundationpress_before_content' ); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+		<section class="main-content" role="main">
+			<?php the_content(); ?>
+		</section>
+		<?php endwhile;?>
+		<?php do_action( 'foundationpress_after_content' ); ?>
+		<?php get_sidebar(); ?>
+	</div>
 <?php get_footer();
