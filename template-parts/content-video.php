93789344a0b87a0f136f2994451ead58e18dc14d
Ben Owen <ben@Bens-MacBook-Pro.local>
2017-08-23 15:49:50 -0500

    video

--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -16,7 +16,21 @@
 		<?php foundationpress_entry_meta(); ?>
 	</header>
 	<div class="entry-content embed-container">
-		<?php the_field('video_url'); ?>
+		<?php
+		// get iframe HTML
+		$iframe = get_field('video_url');
+
+
+		// use preg_match to find iframe src
+		preg_match('/src="(.+?)"/', $iframe, $matches);
+		$src = $matches[1];
+
+		if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $src, $match)) {
+		    $video_id = $match[1];
+		}
+
+		echo '<img src="http://i3.ytimg.com/vi/' . $video_id . '/maxresdefault.jpg"/>';
+		// echo $iframe; ?>
 	</div>
 	<footer>
 		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
