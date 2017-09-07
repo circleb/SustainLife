83f47a62507f0d7f0ec67bee0ad5fcc6f855135f
Ben Owen <ben@Bens-iMac-2.local>
2017-09-07 12:09:04 -0500

    Changed product archive page to show category featured image instead of first product FI.

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -1,9 +1,19 @@
 <?php
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
-if ( has_post_thumbnail( $post->ID ) && !is_product() ) { ?>
+if ( has_post_thumbnail( $post->ID ) && !is_product() && !is_product_category() ) { ?>
 	<header class="featured-hero" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
 	</header>
-<?php } else { ?>
+<?php } else if ( is_product_category() ) {
+		$cat = $wp_query->get_queried_object();
+	    $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
+	    $image = wp_get_attachment_url( $thumbnail_id );
+	    if ( $image ) { ?>
+			<header class="featured-hero" role="banner" data-interchange="[<?php echo $image; ?>, xlarge]">
+			</header>
+		<?php } else { ?>
+			<header class="featured-hero hero-no-image" role="banner"></header>
+		<?php }
+	} else { ?>
 	<header class="featured-hero hero-no-image" role="banner"></header>
 <?php }
