43cd09f845393623c650afb95dfb1297ea43fe7a
Ben Owen <ben@Bens-MacBook-Pro.local>
2017-12-06 17:52:25 -0600

    Added teachers archive page.

---
+++ template-parts/content-teacher.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry, clearfix'); ?>>
+	<blockquote>
+		<img class="img-round float-left teacher-archive-thumb" src="<?php echo the_post_thumbnail_url('thumbnail'); ?>" />
+		<h3><?php the_title(); ?></h3>
+		<?php the_content(); ?>
+	</blockquote>
+</div>
