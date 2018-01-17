73f10c7b8c1c2e39faa189159f95a1f3fc4809e7
Ben Owen <circleb@users.noreply.github.com>
2018-01-17 12:08:30 -0600

    Started page template for listing all classes

--- page-templates/page-all-classes.php
+++ page-templates/page-all-classes.php
@@ -12,32 +12,109 @@ get_header(); ?>
 		<header>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 		</header>
+
+		<?php while ( have_posts() ) : the_post(); ?>
+		<section class="main-content" role="main">
+			<?php the_content(); ?>
+		</section>
+		<?php endwhile;?>
+
 		<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 		<div class="entry-content">
+
 			<?php
-			  $archive_args = array(
-				  'orderby' => 'name',
-				  'order' => 'ASC',
-				  'post_type' => 'teacher',
-				  'posts_per_page'=> -1
+			  $topCatObj = get_term_by( 'slug', 'classes', 'product_cat' );
+			  $topCatID = $topCatObj->term_id;
+
+			  $taxonomy     = 'product_cat';
+			  $orderby      = 'name';
+			  $show_count   = 0;      // 1 for yes, 0 for no
+			  $pad_counts   = 0;      // 1 for yes, 0 for no
+			  $hierarchical = 1;      // 1 for yes, 0 for no
+			  $title        = '';
+			  $empty        = 0;
+
+			  $args = array(
+			         'taxonomy'     => $taxonomy,
+					 'child_of'     => 0,
+					 'parent'       => $topCatID,
+			         'orderby'      => $orderby,
+			         'show_count'   => $show_count,
+			         'pad_counts'   => $pad_counts,
+			         'hierarchical' => $hierarchical,
+			         'title_li'     => $title,
+			         'hide_empty'   => $empty
 			  );
-			  $archive_query = new WP_Query( $archive_args );
+			 $all_categories = get_categories( $args );
+			 echo '<ul>';
+			 foreach ($all_categories as $cat) {
+		        $category_id = $cat->term_id;
+		        echo '<li><a href="' . get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a></li>';
+
+		        $args2 = array(
+		                'taxonomy'     => $taxonomy,
+		                'child_of'     => 0,
+						'parent'       => $category_id,
+		                'orderby'      => $orderby,
+		                'show_count'   => $show_count,
+		                'pad_counts'   => $pad_counts,
+		                'hierarchical' => $hierarchical,
+		                'title_li'     => $title,
+		                'hide_empty'   => $empty
+		        );
+		        $sub_cats = get_categories( $args2 );
+		        if($sub_cats) {
+					echo '<ul>';
+		            foreach($sub_cats as $sub_category) {
+	   			        $sub_cat_id = $sub_category->term_id;
+	   			        echo '<li><a href="' . get_term_link($sub_category->slug, 'product_cat') .'">'. $sub_category->name .'</a></li>';
+
+	   			        $args2 = array(
+	   			                'taxonomy'     => $taxonomy,
+	   			                'child_of'     => 0,
+	   							'parent'       => $sub_cat_id,
+	   			                'orderby'      => $orderby,
+	   			                'show_count'   => $show_count,
+	   			                'pad_counts'   => $pad_counts,
+	   			                'hierarchical' => $hierarchical,
+	   			                'title_li'     => $title,
+	   			                'hide_empty'   => $empty
+	   			        );
+	   			        $sub_sub_cats = get_categories( $args2 );
+	   			        if($sub_sub_cats) {
+	   						echo '<ul>';
+	   			            foreach($sub_sub_cats as $sub_sub_category) {
+								echo '<li><a href="' . get_term_link($sub_sub_category->slug, 'product_cat') .'">'. $sub_sub_category->name .'</a></li>';
+								  	$archive_args = array(
+									  	'orderby' => 'name',
+									  	'order' => 'ASC',
+									  	'post_type' => 'product',
+										//  	'product_cat' => $sub_sub_category->slug,
+									  	'posts_per_page'=> -1,
+										'tax_query'     => array(
+											array(
+												'taxonomy'  => 'product_cat',
+												'field'     => 'id',
+												'terms'     => $sub_sub_category->term_id
+											)
+										)
+								  	);
+								  	$archive_query = new WP_Query( $archive_args );
+									if ( $archive_query->have_posts() ) {
+										echo $sub_sub_category->term_id;
+										while ( $archive_query->have_posts() ) : $archive_query->the_post();
+											echo 'Post Loop Test';
+										endwhile;
+									}
+	   			            }
+	   						echo '</ul>';
+	   			        }
+	   				}
+					echo '</ul>';
+		        }
+			}
+			echo '</ul>';
 			?>
-			<?php if ( $archive_query->have_posts() ) : ?>
-			<div class="row teacher-masonry-container small-up-1 medium-up-2 align-center">
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( $archive_query->have_posts() ) : $archive_query->the_post(); // run the custom loop ?>
-					<div class="column">
-						<?php get_template_part( 'template-parts/content', 'teacher' ); ?>
-					</div>
-				<?php endwhile; ?>
-
-				<?php else : ?>
-					<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-				<?php endif; // End have_posts() check. ?>
-			</div>
 		</div>
 		<footer>
 		</footer>
---
+++ template-parts/content-classlisting.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
+	<header>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	</header>
+</div>
