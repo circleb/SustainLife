0aa8a785f7bc164437f6a2ce2e06e786822cd540
Ben Owen <circleb@users.noreply.github.com>
2018-02-01 16:33:07 -0600

    Changed Category Slider to Category Block

--- library/visualcomposer.php
+++ library/visualcomposer.php
@@ -1,7 +1,7 @@
 <?php
 vc_map( array(
    "name" => __("SL Category Slider"),
-   "base" => "sl-catslider",
+   "base" => "sl-catblock",
    "icon" => get_template_directory_uri() . "/assets/images/ploughshare-mobile-logo.png",
    "category" => __('Content'),
    "params" => array(
@@ -9,26 +9,46 @@ vc_map( array(
          'type' => 'textfield',
          'heading' => 'Category ID',
          'param_name' => 'category',
+     ),
+     array(
+         'type' => 'textfield',
+         'heading' => 'Number of Columns',
+         'param_name' => 'columns',
+     ),
+     array(
+         'type' => 'textarea',
+         'heading' => 'Category Description',
+         'param_name' => 'description',
      )
    )
 ));
 
-add_shortcode( 'sl-catslider', 'sl_catslider_handler' );
+add_shortcode( 'sl-catblock', 'sl_catblock_handler' );
 
-function sl_catslider_handler( $atts, $content = null ) {
+function sl_catblock_handler( $atts, $content = null ) {
     extract(shortcode_atts(array(
-        "category" => '21'
+        "category" => '21',
+        "description" => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        "columns" => '3'
     ), $atts));
-    $sct = 1;
-    $pct = 1;
     $categories = get_categories(array('taxonomy' => 'product_cat', 'parent' => $category));
     $catinfo = get_term( $category );
 
-    $html = '<h2 class="product-category-slider-title"><a href="' . get_term_link( $catinfo->slug, 'product_cat' ) . '">' . $catinfo->name . '</a></h2>';
-    $html .= '<div class="product-category-slider orbit" data-orbit>
-                <ul class="orbit-container">
-                    <li class="is-active orbit-slide">
-                    <div class="row small-up-2 medium-up-3 align-center">';
+    $html = '<div class="product-category-block">';
+    $html .= '<h2 class="product-category-block-title"><a href="' . get_term_link( $catinfo->slug, 'product_cat' ) . '">' . $catinfo->name . '</a></h2>';
+    $html .= '<div class="product-category-block-catlist">';
+    $cati = 0;
+    $catcount = count($categories);
+    foreach( $categories as $category ) {
+        $html .= '<a href="'. get_term_link($category->slug, 'product_cat') .'">' . $category->name . '</a>';
+        if ($cati != $catcount - 1) {
+            $html .= ' • ';
+        }
+        $cati++;
+    }
+    $html .= '</div>';
+    $html .= '<p class="product-category-block-description">' . $description . '</p>';
+    $html .= '<div class="row small-up-1 medium-up-' . $columns . ' align-center">';
 
                 foreach( $categories as $category ) {
                     $catid = $category->term_id;
@@ -36,46 +56,31 @@ function sl_catslider_handler( $atts, $content = null ) {
                     $thumbnail_id = get_woocommerce_term_meta( $catid, 'thumbnail_id', true );
                     $image = wp_get_attachment_image( $thumbnail_id, array(310,310) );
 
-                    if ($sct == 4) {
-                        ++$pct;
-                        $sct = 1;
-                        $html .= '</div>
-                        </li>
-                        <li class="is-active orbit-slide">
-                        <div class="row small-up-2 medium-up-3 align-center">';
-                    }
                     $html .= '
                     <div class="column">
-                        <div class="category-card">
-                            <div class="category-card-thumbnail">
-                                ' . $image . '
-                            </div>
-                            <div class="category-card-info">
-                                <h3><a href="'. get_term_link($category->slug, 'product_cat') .'">' . $category->name . '</a></h3>';
-                                if (!empty($subcategories)) {
-                                    foreach( $subcategories as $subcategory ) {
-                                        $html .= '<p><a href="'. get_term_link($subcategory->slug, 'product_cat') .'">' . $subcategory->name . '</a></p>';
-                                    }
-                                } else {
-                                    $args = array( 'post_type' => 'product', 'posts_per_page' => -1, 'product_cat' => $category->name );
-                                    $loop = new WP_Query( $args );
-                                    while ( $loop->have_posts() ) : $loop->the_post();
-                                        global $product;
-                                        $html .= '<p><a href="'.get_permalink().'">' . get_the_title() . '</a></p>';
-                                    endwhile;
-                                    wp_reset_query();
-                                }
-                    $html .= '</div>
+                        <div class="category-card-thumbnail">
+                            ' . $image . '
                         </div>
+                        <div class="category-card-info">
+                            <h3><a href="'. get_term_link($category->slug, 'product_cat') .'">' . $category->name . '</a></h3>';
+                            if (!empty($subcategories)) {
+                                foreach( $subcategories as $subcategory ) {
+                                    $html .= '<p><a href="'. get_term_link($subcategory->slug, 'product_cat') .'">' . $subcategory->name . '</a></p>';
+                                }
+                            } else {
+                                $args = array( 'post_type' => 'product', 'posts_per_page' => -1, 'product_cat' => $category->name );
+                                $loop = new WP_Query( $args );
+                                while ( $loop->have_posts() ) : $loop->the_post();
+                                    global $product;
+                                    $html .= '<p><a href="'.get_permalink().'">' . get_the_title() . '</a></p>';
+                                endwhile;
+                                wp_reset_query();
+                            }
+                    $html .= '</div>
                     </div>';
-                    ++$sct;
                 }
-    $html .= '</div></li>';
-    if ($pct > 1) {
-        $html .= '<div class="orbit-controls"><button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button></div>';
-    }
-    $html .= '</ul></div>';
+    $html .= '</div>';
+    $html .= '</div>';
 
 return $html;
 
