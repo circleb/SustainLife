a1eac0003c563c48936e34737fda8cf8f833e991
Ben Owen <ben@sustainlife.org>
2018-04-13 11:07:22 -0500

    Photo Gallery

--- assets/javascript/custom/random.js
+++ assets/javascript/custom/random.js
@@ -4,7 +4,7 @@
     if ( $('.products .type-product').length ) {
         $('.products .type-product .price').each(function() {
             if ($(this).text().indexOf('–') > -1) {
-                var highPrice = $(this).find(">:last-child").remove();
+                var highPrice = $(this).find(">:last-child");
                 $(this).html(highPrice);
             }
         });
