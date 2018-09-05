2c48bf33f68746aa13d0638811a2e171250fcb95
Matthew Pressly <matthew.pressly@gmail.com>
2018-09-05 12:52:01 -0500

    Made copyright year update automatically.

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 			</footer>
 			<footer class="sub-footer">
 				<div class="foot-content">
-					© 2017 Ploughshare Institute for Sustainable Culture. All rights reserved.
+					&copy; <?php echo date("Y"); ?> Ploughshare Institute for Sustainable Culture. All rights reserved.
 				</div>
 			</footer>
 		</div>
